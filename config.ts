44725f7016351f83136b3f8961a2397919339861
Bas Pennings <baspster@gmail.com>
2016-05-18 19:56:32 +0200

    Working middleware-based bot

---
+++ config.ts
@@ -0,0 +1,11 @@
+const cfg = {
+    username: 'Methbot',
+    password: 'Foo123',
+    nick: 'Methbot',
+    channel: '##vanityguild',
+    user: 'https://github.com/basp/methbot'    
+};
+
+export {
+    cfg
+}
--- index.ts
+++ index.ts
@@ -4,44 +4,84 @@ import _ = require('lodash');
 import net = require('net');
 import irc = require('slate-irc');
 
-// https://github.com/csauve/node-greenman
+import {cfg} from './config';
 
-
-// const stream = net.connect({
-//     port: 6667,
-//     host: 'irc.freenode.org'
-// });
-
-// const client = irc(stream);
-
-
-
-// https://github.com/Zlobin/es-middleware
-class Bot {
-    use(fn) {
-        this.run = (stack =>
-            next =>
-                stack(() =>
-                    fn.call(this, next.bind(this))
-                )
-        )(this.run);
-
-        return this;
-    }
-
-    public run = next => next();
+interface NamesEvent {
+    channel: string;
+    names: { name: string, mode: string }[];
 }
 
-const bot = new Bot();
+declare module "slate-irc" {
+    interface Client {
+        on(event: 'names', callback: (e: NamesEvent) => void);
+    }
+}
 
-bot.use(next => {
-    console.log('foo');
-    next();
+type Middleware = (data: any, next: () => void) => void;
+type Stack = Middleware[];
+
+class Bot {
+    private stacks = {};
+    private client: irc.Client;
+    private cfg: any;
+
+    constructor(client: irc.Client) {
+        this.client = client;
+
+        var events = [
+            'data',
+            'welcome',
+            'message',
+            'notice',
+            'motd',
+            'join',
+            'part',
+            'nick',
+            'quit',
+            'names',
+        ];
+
+        events.forEach(n => this.client.on(n, data => {
+            if (!this.stacks[n]) return;
+            this.run(data, this.stacks[n]);
+        }));
+    }
+
+    use(event: string, fn: Middleware) {
+        if (!this.stacks[event]) this.stacks[event] = [];
+        this.stacks[event].push(fn);
+    }
+
+    connect(cfg) {
+        this.cfg = cfg;
+
+        this.client.nick(cfg.nick);
+        this.client.pass(cfg.password);
+        this.client.user(cfg.username, cfg.user);
+        this.client.join(cfg.channel);
+    }
+
+    private run(data: any, stack: Middleware[]) {
+        let cont = false;
+        let next = () => { cont = true };
+        for (var i = 0; i < stack.length; i++) {
+            stack[i](data, next);
+            if (!cont) break;
+        }
+    }
+}
+
+const stream = net.connect({
+    port: 6667,
+    host: 'irc.freenode.org'
 });
 
-bot.use(next => {
-    console.log('bar');
-    next();
-});
+const client = irc(stream);
 
-bot.run(() => console.log('quux'));
+const bot = new Bot(client);
+
+// bot.use('data', (x: irc.DataEvent) => console.log(x));
+
+bot.use('names', (x: NamesEvent) => console.log(x.names));
+bot.use('names', (x: NamesEvent) => console.log(x.channel));
+bot.connect(cfg);
