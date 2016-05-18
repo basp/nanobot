b4c9db6b622792a5793a77464d5510657a02b528
unknown <bas.pennings@SPARE16.kantoor.tld>
2016-05-18 16:55:32 +0200

    Reference link

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 /// <reference path="typings/main.d.ts" />
 
+import _ = require('lodash');
 import net = require('net');
 import irc = require('slate-irc');
 
-const stream = net.connect({
-    port: 6667,
-    host: 'irc.freenode.org'
-});
+// const stream = net.connect({
+//     port: 6667,
+//     host: 'irc.freenode.org'
+// });
 
-const client = irc(stream);
+// const client = irc(stream);
 
 // https://github.com/Zlobin/es-middleware
 // https://github.com/csauve/node-greenman
