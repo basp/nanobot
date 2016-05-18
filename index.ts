9a18e16c15925356fbc809b742ed9a91074bdd4b
Bas Pennings <baspster@gmail.com>
2016-05-18 18:16:17 +0200

    Basic middleware example

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import _ = require('lodash');
 import net = require('net');
 import irc = require('slate-irc');
 
+// https://github.com/csauve/node-greenman
+
+
 // const stream = net.connect({
 //     port: 6667,
 //     host: 'irc.freenode.org'
@@ -11,6 +14,34 @@ import irc = require('slate-irc');
 
 // const client = irc(stream);
 
-// https://github.com/Zlobin/es-middleware
-// https://github.com/csauve/node-greenman
 
+
+// https://github.com/Zlobin/es-middleware
+class Bot {
+    use(fn) {
+        this.run = (stack =>
+            next =>
+                stack(() =>
+                    fn.call(this, next.bind(this))
+                )
+        )(this.run);
+
+        return this;
+    }
+
+    public run = next => next();
+}
+
+const bot = new Bot();
+
+bot.use(next => {
+    console.log('foo');
+    next();
+});
+
+bot.use(next => {
+    console.log('bar');
+    next();
+});
+
+bot.run(() => console.log('quux'));
