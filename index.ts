be3b512a4abb266114507907abd4bf57c9b3fa24
unknown <bas.pennings@SPARE16.kantoor.tld>
2016-05-18 15:57:19 +0200

    Basic project setup

---
+++ index.ts
@@ -0,0 +1,15 @@
+/// <reference path="typings/main.d.ts" />
+
+import net = require('net');
+import irc = require('slate-irc');
+
+const stream = net.connect({
+    port: 6667,
+    host: 'irc.freenode.org'
+});
+
+const client = irc(stream);
+
+// https://github.com/Zlobin/es-middleware
+// https://github.com/csauve/node-greenman
+
