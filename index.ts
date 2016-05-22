4d5ad4a12cfd1e4f6c02be52a1cf3ad61a39aeff
unknown <bas.pennings@SPARE16.kantoor.tld>
2016-05-22 12:12:59 +0200

    Rewriting using promises

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import net = require('net');
 
 import {Bot} from './nanobot';
 import {cfg} from './config';
+import {init} from './markov-responder';
 
 var speak = require('speakeasy-nlp');
 
@@ -15,20 +16,40 @@ const stream = net.connect({
 const client = irc(stream);
 const bot = new Bot(client);
 
-bot.use('data', data => {
-    console.log(data);
+function logMarkovProgress(path: string): void {
+    console.log(`Loaded ${path}`);
+}
+
+RSVP.hash({
+    cat: init('c:/temp/cat.txt', 4, logMarkovProgress),
+    chat: init('c:/temp/chat-sanitized.log', 2, logMarkovProgress),
+    plato: init('c:/temp/plato.txt', 3, logMarkovProgress),
+    shakespeare: init('c:/temp/shakespeare.txt', 2, logMarkovProgress)
+}).then(r => {
+    const cat: Markov = r.cat;
+    const chat: Markov = r.chat;
+    const plato: Markov = r.plato;
+    const shakespeare: Markov = r.shakespeare;
+
+    bot.use('data', data => {
+        console.log(data);
+    });
+
+    bot.use('message', (e: irc.MessageEvent, next) => {
+        const c = speak.classify(e.message);
+        const s = speak.sentiment.analyze(e.message);
+        console.log(c);
+        console.log(`SCORE: ${s.score}`);
+        next();
+    });
+
+    bot.msg(/meth|methbot/, (e: irc.MessageEvent, m) => {
+        const msg = plato.respond(e.message)
+            .map(x => x.trim())
+            .join(' ');
+        bot.say(cfg.channel, msg);
+    });
+
+    bot.connect(cfg);
 });
 
-bot.use('message', (e: irc.MessageEvent, next) => {
-    const c = speak.classify(e.message);
-    const s = speak.sentiment.analyze(e.message);
-    console.log(c);
-    console.log(`SCORE: ${s.score}`);
-    next();
-});
-
-bot.msg(/meth|methbot/, (e: irc.MessageEvent, m) => {
-    
-});
-
-bot.connect(cfg);
